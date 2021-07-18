2477a7aa211e615112309857c8ecc448f8d0d035
Aleksey <rubleuski.a@gmail.com>
2021-07-18 15:28:50 +0300

    SP

---
+++ WebApplication4/Areas/Identity/Data/WebApplication4Context.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApplication4.Areas.Identity.Data;
+
+namespace WebApplication4.Data
+{
+    public class WebApplication4Context : IdentityDbContext<WebApplication4User>
+    {
+        public WebApplication4Context(DbContextOptions<WebApplication4Context> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ WebApplication4/Areas/Identity/Data/WebApplication4User.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace WebApplication4.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the WebApplication4User class
+    public class WebApplication4User : IdentityUser
+    {
+    }
+}
