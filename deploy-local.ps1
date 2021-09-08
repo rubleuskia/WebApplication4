4ff923865fadec6fa4f5f1132c938ce6a6ff6f0a
Aleksey Rublevsky <aliakseiru@playtika.com>
2021-09-08 20:02:09 +0300

    Add stub heroku package json

--- deploy-local.ps1
+++ deploy-local.ps1
@@ -1,10 +1,3 @@
-Set-Location -Path "./WebApplication4/wwwroot/angular"
-
-& npm i
-& npm run build
-
-Set-Location -Path "./../../../"
-
 & dotnet publish -o ./deploy
 
 Set-Location -Path "./deploy/"
